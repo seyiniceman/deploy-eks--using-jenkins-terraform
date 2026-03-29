5171980b702a422a7c8771b83525d64e9c01a889
Seyi Akinmusere <seyiniceman@gmail.com>
2026-03-29 11:32:09 +0100

    Modify EKS cluster configuration for AWS provider v6
    Updated AWS provider version and adjusted Kubernetes provider configuration.

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,34 +1,45 @@
+# 1. ADD THIS BLOCK FIRST: Tells Terraform to use the newer AWS provider
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0" 
+    }
+  }
+}
+
 provider "kubernetes" {
-    #load_config_file = "false"
-    host = data.aws_eks_cluster.myapp-cluster.endpoint
-    token = data.aws_eks_cluster_auth.myapp-cluster.token
+    host                   = data.aws_eks_cluster.myapp-cluster.endpoint
+    token                  = data.aws_eks_cluster_auth.myapp-cluster.token
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.myapp-cluster.certificate_authority.0.data)
 }
 
 data "aws_eks_cluster" "myapp-cluster" {
-    name = module.eks.cluster_name
+    name       = module.eks.cluster_name
     depends_on = [module.eks]
 }
-
 
 data "aws_eks_cluster_auth" "myapp-cluster" {
-    name = module.eks.cluster_name
+    name       = module.eks.cluster_name
     depends_on = [module.eks]
 }
+
 output "cluster_id" {
   value = data.aws_eks_cluster.myapp-cluster.id
 }
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "20.26.0" 
+  # Fixes compatibility with AWS Provider v6
+  version = "~> 21.0" 
 
-  cluster_name = "myapp-eks-cluster"
+  cluster_name    = "myapp-eks-cluster"
   cluster_version = "1.30"
 
   subnet_ids = module.myapp-vpc.private_subnets
-  vpc_id = module.myapp-vpc.vpc_id
-  cluster_endpoint_public_access = true
+  vpc_id     = module.myapp-vpc.vpc_id
+  
+  cluster_endpoint_public_access           = true
   enable_cluster_creator_admin_permissions = true
 
   tags = {
@@ -43,8 +54,9 @@ module "eks" {
       desired_size = 3
 
       instance_types = ["t2.small"]
-      key_name       = "sfkeypair"
+      key_name       = "sf_key"
     }
   }
-depends_on = [module.myapp-vpc]
+  
+  depends_on = [module.myapp-vpc]
 }
