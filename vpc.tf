4194d0418c7a4a46415f833638c9fd63739f6e2c
Seyi Akinmusere <seyiniceman@gmail.com>
2026-03-29 13:04:00 +0100

    Update VPC module and add necessary tags
    Refactor VPC module configuration and add tags for EKS.

--- vpc.tf
+++ vpc.tf
@@ -1,36 +1,58 @@
-variable vpc_cidr_block {}
-variable private_subnet_cidr_blocks {}
-variable public_subnet_cidr_blocks {}
+############################################
+# Variables
+############################################
+variable "vpc_cidr_block" {}
+variable "private_subnet_cidr_blocks" {}
+variable "public_subnet_cidr_blocks" {}
 
-data "aws_availability_zones" "available" {}
+############################################
+# Availability Zones (Filtered)
+############################################
+data "aws_availability_zones" "available" {
+  state = "available"
+}
 
-
+############################################
+# VPC Module
+############################################
 module "myapp-vpc" {
-    source =   "terraform-aws-modules/vpc/aws"
-    version = "5.14.0"  #"5.0.0"
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.14"
 
-    name = "myapp-vpc"
-    cidr = var.vpc_cidr_block
-    private_subnets = var.private_subnet_cidr_blocks
-    public_subnets = var.public_subnet_cidr_blocks
-    azs = data.aws_availability_zones.available.names 
-    
-    enable_nat_gateway = true
-    single_nat_gateway = true
-    enable_dns_hostnames = true
+  name = "myapp-vpc"
+  cidr = var.vpc_cidr_block
 
-    tags = {
-        "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
-    }
+  # Ensure AZ count matches subnet count
+  azs = slice(
+    data.aws_availability_zones.available.names,
+    0,
+    length(var.private_subnet_cidr_blocks)
+  )
 
-    public_subnet_tags = {
-        "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
-        "kubernetes.io/role/elb" = 1 
-    }
+  private_subnets = var.private_subnet_cidr_blocks
+  public_subnets  = var.public_subnet_cidr_blocks
 
-    private_subnet_tags = {
-        "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
-        "kubernetes.io/role/internal-elb" = 1 
-    }
+  ############################################
+  # Networking
+  ############################################
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
 
+  ############################################
+  # Tags for EKS (VERY IMPORTANT)
+  ############################################
+  tags = {
+    "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
+    "kubernetes.io/role/elb"                  = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
+    "kubernetes.io/role/internal-elb"         = "1"
+  }
 }
