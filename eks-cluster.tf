8fd960526d135cb340f938d5002f702452e61c11
Seyi Akinmusere <seyiniceman@gmail.com>
2026-04-01 23:21:00 +0100

    Add aws_profile variable and update EKS configuration

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -10,8 +10,15 @@ terraform {
   }
 }
 
+# Variable to allow local use with "Seyi" but empty for Jenkins
+variable "aws_profile" {
+  type    = string
+  default = "" 
+}
+
 provider "aws" {
-  region = "eu-west-1"
+  region  = "eu-west-1"
+  profile = var.aws_profile 
 }
 
 ############################################
@@ -27,28 +34,35 @@ module "eks" {
   vpc_id     = module.myapp-vpc.vpc_id
   subnet_ids = module.myapp-vpc.private_subnets
 
-  endpoint_public_access                   = true
+  endpoint_public_access = true
+  
   enable_cluster_creator_admin_permissions = true
+  authentication_mode                      = "API_AND_CONFIG_MAP"
 
-  # FIX: Grants your IAM user permission to view/manage nodes in the AWS Console
-  access_entries = {
-    console_access = {
-      principal_arn     = "arn:aws:iam::021891594207:user/myownyolk"
-      policy_associations = {
-        admin = {
-          policy_arn = "arn:aws:iam::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-          access_scope = {
-            type = "cluster"
-          }
-        }
-      }
+  ############################################
+  # FIX: Using 'addons' as requested
+  ############################################
+  addons = {
+    vpc-cni = {
+      most_recent    = true
+      before_compute = true 
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    coredns = {
+      most_recent = true
+    }
+    eks-pod-identity-agent = {
+      most_recent = true
     }
   }
 
-  # FIX: Ensures the new KMS key allows the nodes to talk to the cluster
   create_kms_key                = true
   kms_key_enable_default_policy = true
   kms_key_administrators        = ["arn:aws:iam::021891594207:root"]
+  
+  create_cloudwatch_log_group = false
 
   tags = {
     environment = "development"
@@ -63,7 +77,6 @@ module "eks" {
       desired_size   = 3
       key_name       = "sf_key"
 
-      # FIX: Ensures the CNI has permission to assign networking to the nodes
       iam_role_additional_policies = {
         AmazonEKS_CNI_Policy = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
       }
@@ -81,6 +94,7 @@ provider "kubernetes" {
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
+    # FIX: Removed --profile Seyi so Jenkins credentials work
     args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
   }
 }
@@ -88,10 +102,5 @@ provider "kubernetes" {
 ############################################
 # 4. Outputs
 ############################################
-output "cluster_name" {
-  value = module.eks.cluster_name
-}
-
-output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
-}
+output "cluster_name" { value = module.eks.cluster_name }
+output "cluster_endpoint" { value = module.eks.cluster_endpoint }
